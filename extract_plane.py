d7bc10c9ccfb2912b160dc6b51d75a50d79c8a7d
Jarrett Bolander <010.git@endor.cyou>
2025-04-14 20:28:11 +0200

    Add voxelize() and extract_plane()

--- extract_plane.py
+++ extract_plane.py
@@ -8,6 +8,7 @@ import gen
 import matplotlib.pyplot as plt
 
 
+# %% setup
 def extract_plane(scene, plane=3, dist=0.1):
     """
     Extract the portion of the scene point cloud that is near the
@@ -24,78 +25,79 @@ def extract_plane(scene, plane=3, dist=0.1):
         numpy array: narrow cross section of the scene
     """
 
-    print("shape of scene pcd: \n", ship.shape)
-    slice = ship[np.abs(ship[:, 0] - plane) < dist]
+    print("shape of scene pcd: \n", scene.shape)
+    slice = scene[np.abs(ship[:, 0] - plane) < dist]
+    slice[:, 0] = 0
+    return slice
 
 
-# %% setup
-density = 5
-ship = np.concatenate(
-    (
-        gen.plane(origin=[5, 5, 5], length=10, width=2, density=density),
-        gen.plane(
-            origin=[5, 5, 5], length=10, width=2, roll=np.pi / 2, density=density
-        ),
-        gen.plane(
-            origin=[5, 5, 5], length=10, width=2, pitch=np.pi / 2, density=density
-        ),
-        gen.plane(origin=[0, 10, 30], length=110, width=90, density=density),
-        gen.tbeam(origin=[0, 30, 30], length=90, density=density),
-        gen.tbeam(origin=[0, 60, 30], length=90, density=density),
-        gen.tbeam(origin=[0, 90, 30], length=90, density=density),
-        gen.curved_wall(
-            origin=[110, 5, 0],
-            length=110,
-            height=30,
-            yaw=np.pi / 2,
-        ),
-        gen.bulb_flat(origin=[0, 10, 30], roll=np.pi / 2, yaw=np.pi / 2),
-        gen.plane(origin=[0, 0, 0], length=110, width=90, density=density),
-        gen.ibeam(origin=[20, 20, 5], roll=np.pi / 2),
+def voxelize(slice, res=5):
+    """
+    Voxelize a two dimensional point cloud projection to create a binary image
+    Assumes projected cloud lies on a yz plane
+
+    Args:
+        slice (numpy array): two dimensional point cloud projection
+        res (int): number of squares per unit distance in the image
+
+    Returns:
+        grid (numpy array): two dimensional binary image
+    """
+
+    min_y = slice[:, 1].min()
+    min_z = slice[:, 2].min()
+    max_y = slice[:, 1].max()
+    max_z = slice[:, 2].max()
+    grid_y = int((max_y - min_y) * res)
+    grid_z = int((max_z - min_z) * res)
+    grid = np.zeros([grid_y, grid_z])
+    grid_idx_y = np.linspace(min_y, max_y, int((max_y - min_y) * res))
+    grid_idx_z = np.linspace(min_z, max_z, int((max_z - min_z) * res))
+    for point in slice:
+        idx_y = np.argmin(np.abs(point[1] - grid_idx_y))
+        idx_z = np.argmin(np.abs(point[2] - grid_idx_z))
+        grid[idx_y, idx_z] = 1
+        print("changing grid at", idx_z, ", ", idx_y, " to 1")
+    return grid
+
+
+# %% main
+if __name__ == "__main__":
+    density = 3
+    ship = np.concatenate(
+        (
+            gen.plane(origin=[5, 5, 5], length=10, width=2, density=density),
+            gen.plane(
+                origin=[5, 5, 5], length=10, width=2, roll=np.pi / 2, density=density
+            ),
+            gen.plane(
+                origin=[5, 5, 5], length=10, width=2, pitch=np.pi / 2, density=density
+            ),
+            gen.plane(origin=[0, 10, 30], length=110, width=90, density=density),
+            gen.tbeam(origin=[0, 30, 30], length=90, density=density),
+            gen.tbeam(origin=[0, 60, 30], length=90, density=density),
+            gen.tbeam(origin=[0, 90, 30], length=90, density=density),
+            gen.curved_wall(
+                origin=[110, 5, 0],
+                length=110,
+                height=30,
+                yaw=np.pi / 2,
+            ),
+            gen.bulb_flat(origin=[0, 10, 30], roll=np.pi / 2, yaw=np.pi / 2),
+            gen.plane(origin=[0, 0, 0], length=110, width=90, density=density),
+            gen.ibeam(origin=[20, 20, 5], roll=np.pi / 2),
+        )
     )
-)
-print("shape of pcd: \n", ship.shape)
-plane = 3  # plane perpendiculat to the y axis at 3 along it
-dist = 1.4
-slice = ship[np.abs(ship[:, 0] - plane) < dist]
-slice[:, 0] = 0
+    slice = extract_plane(ship)
+    grid = voxelize(slice)
+    plt.pcolor(grid.T, cmap="binary", edgecolors="r")
+    plt.show()
 
-# %% voxelize
-print("slice.max is ", slice[:, 0].max())
-print("slice.max is ", slice[:, 1].max())
-print("slice.max is ", slice[:, 2].max())
+    slice_pcd = o3d.geometry.PointCloud()
+    slice_pcd.points = o3d.utility.Vector3dVector(slice)
+    slice_voxel = o3d.geometry.VoxelGrid.create_from_point_cloud(slice_pcd, 0.3)
 
-
-min_y = slice[:, 1].min()
-min_z = slice[:, 2].min()
-max_y = slice[:, 1].max()
-max_z = slice[:, 2].max()
-
-res = 5
-grid_y = int((max_y - min_y) * res)
-grid_z = int((max_z - min_z) * res)
-grid = np.zeros([grid_y, grid_z])
-grid_idx_y = np.linspace(min_y, max_y, int((max_y - min_y) * res))
-grid_idx_z = np.linspace(min_z, max_z, int((max_z - min_z) * res))
-for point in slice:
-    # put point in closest grid point
-    # when a grid point has a pcd point, we set that grid pixel to 1
-    idx_y = np.argmin(np.abs(point[1] - grid_idx_y))
-    idx_z = np.argmin(np.abs(point[2] - grid_idx_z))
-    grid[idx_y, idx_z] = 1
-    print("changing grid at", idx_z, ", ", idx_y, " to 1")
-print("grid:\n", grid)
-print("shape of grid:\n", np.shape(grid))
-print("slice[2]: ", slice[2])
-plt.pcolor(grid.T, cmap="binary", edgecolors="r")
-plt.show()
-
-slice_pcd = o3d.geometry.PointCloud()
-slice_pcd.points = o3d.utility.Vector3dVector(slice)
-slice_voxel = o3d.geometry.VoxelGrid.create_from_point_cloud(slice_pcd, 0.3)
-
-# df  = x +
-print("shape of pcd: \n", slice.shape)
-pcd = o3d.t.geometry.PointCloud(slice)
-pcd2 = o3d.t.geometry.PointCloud(ship)
-o3d.visualization.draw_geometries([slice_voxel])
+    print("shape of pcd: \n", slice.shape)
+    pcd = o3d.t.geometry.PointCloud(slice)
+    pcd2 = o3d.t.geometry.PointCloud(ship)
+    o3d.visualization.draw_geometries([slice_voxel])
