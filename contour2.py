e0cff044f907f2ef53d15946541b9bdf92c9c603
Jarrett Bolander <010.git@endor.cyou>
2025-05-20 12:57:38 +0200

    Basic contouring

--- contour2.py
+++ contour2.py
@@ -61,7 +61,34 @@ def gen_cloud():
     return grid
 
 
-def get_contour(img):
+def gen_cloud2():
+    density = 10
+    ship = np.concatenate(
+        [
+            gen.plane(origin=[0, 10, 30], length=110, width=90, density=density),
+            gen.plane(origin=[0, 10, 00], length=110, width=90, density=density),
+        ]
+    )
+    slice = ep.extract_plane(ship)
+    grid = ep.voxelize(slice)
+    return grid
+
+
+def gen_cloud3():
+    density = 10
+    ship = np.concatenate(
+        [
+            gen.tbeam(origin=[0, 30, 30], length=90, density=density),
+            gen.tbeam(origin=[0, 60, 30], length=90, density=density),
+            gen.tbeam(origin=[0, 90, 30], length=90, density=density),
+        ]
+    )
+    slice = ep.extract_plane(ship)
+    grid = ep.voxelize(slice)
+    return grid
+
+
+def get_contours(img):
     img = np.uint8(img)
     _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY)
     cntrs, hrrchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -74,11 +101,16 @@ if __name__ == "__main__":
     import gen
     import extract_plane as ep
 
-    img = gen_cloud()
-    cntrs = get_contour(img)
+    structures = []
+    structures.append(gen_cloud2())
+    structures.append(gen_cloud3())
+    cntrs = get_contours(structures[1])
+    # where (x1-x2)^2 + (y1-y2)^2 is minimal, given that x1,y1 is the
+    # last point of the old primitive and x2y2 is the first point of the new primitive
+    # last point of old primitive should be ymax, then xmax, or vice versa. this is primitive specific
 
     # Visualization. Not required
-    w, h = img.shape
+    w, h = structures[0].shape
     viz = np.zeros((w, h))
     cntr = cntrs[0].reshape(-1, 2)
     for point in cntr:
