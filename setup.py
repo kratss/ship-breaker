b8d5c4d0922b8f1c442c79ab28283a2ad563ae91
Jarrett Bolander <010.git@endor.cyou>
2025-07-30 16:26:42 -0600

    Clean up repo

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "PyQt5",
         "icecream",
         "opencv-contrib-python",
+        "networkx",
     ],
     author="Jarrett Bolander",
     description="Autonomous ship dismantling path planning library",
